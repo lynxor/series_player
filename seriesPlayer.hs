bfccf02f0be2e7d0c154c7b00c0697bc404ce64e
Dawid <dawid.malan@gmail.com>
2011-09-24 13:51:53 +0200

    Add support for continuing where you left off

--- seriesPlayer.hs
+++ seriesPlayer.hs
@@ -1,14 +1,19 @@
 import System.Environment
 import System.Directory
+import System.IO
+import System.IO.Error
 import Data.List
 import System.Cmd
+
 import qualified Data.Map as Map  
 
 main = do
-  (episodeNo:other) <- getArgs
+  (arg:other) <- getArgs
   dirs <- getDirectoryContents "."
-  watchEps episodeNo dirs
+  handleArg arg dirs
   
+handleArg "continue" dirs = (continue dirs) `catch` errHandler
+handleArg ep dirs = watchEps ep dirs  
 
 watchEps episodeNo dirs = do
   let results = zip [1 .. ] (filter (findInString episodeNo) dirs)
@@ -18,18 +23,19 @@ watchEps episodeNo dirs = do
   putStrLn $ unlines (map (\a -> option a) results)
   putStrLn "Choose an option dude : "
   chosen <- getLine
-  success <- handleLookup(Map.lookup (read chosen) resultMap)
+  success <- handleLookup (Map.lookup (read chosen) resultMap) episodeNo
   if success 
     then do
            putStrLn "Watch the next episode?"
-           watchEps (show ((read episodeNo) + 1)) dirs 
+           watchEps (epPlusOne episodeNo) dirs 
   else watchEps episodeNo dirs
 
-handleLookup (Just x) = do
+handleLookup (Just x) ep = do
+  writeFile ".series_continue" ep 
   rawSystem "mplayer" [x]
   putStrLn $ "done with " ++ x
   return True
-handleLookup Nothing = do
+handleLookup Nothing ep = do
   putStrLn "Pick something dumbass"
   return False
   
@@ -38,3 +44,20 @@ findInString :: String -> String -> Bool
 findInString needle haystack = foldl foldFunc False (tails haystack)
   where foldFunc acc item = acc || ((take len item) == needle)
         len = length needle
+
+epPlusOne :: String -> String
+epPlusOne ep = show $ (read ep) + 1
+
+continue dirs = do
+  contents <- readFile ".series_continue"
+  let episodeNo = head $ lines contents
+      handle "y" ep = watchEps (epPlusOne ep) dirs
+      handle "n" ep = watchEps ep dirs            
+  putStrLn $ "Did you finish watching " ++ episodeNo ++ " [y/n]"
+  yn <- getLine
+  handle yn episodeNo
+  
+errHandler :: IOError -> IO ()
+errHandler e
+       | isDoesNotExistError e = putStrLn "You do not have a continue file yet"
+       | otherwise = ioError e
