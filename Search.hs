c824b1c44d0f38dab3d5d9c22d0c3bd2505205d0
Dawid <dawid.malan@gmail.com>
2012-02-26 00:52:11 +0200

    ignore files if the number is not the whole number

--- Search.hs
+++ Search.hs
@@ -24,7 +24,7 @@ seriesMatch epNo fileName = if fileName =~ regex then 10 else 0
     where regex = "S[0-9]{2}E0?" ++ (show epNo)
 
 separatedMatch :: Int -> String -> Int
-separatedMatch epNo fileName = if (fileName =~ ("\\s0?" ++ (show epNo) ++ "\\s")) 
+separatedMatch epNo fileName = if (fileName =~ ("\\s0?" ++ (show epNo) ++ "[\\s\\.\\-\\(]")) 
                                   then 10 
                                else 0
 
@@ -32,6 +32,7 @@ endsWith :: String -> String -> Bool
 endsWith needle haystack = haystack =~ (needle ++ "$")
 
 findInString :: String -> String -> Bool
-findInString needle haystack = foldl foldFunc False (tails haystack)
-  where foldFunc acc item = acc || ((take len item) == needle)
-        len = length needle
+findInString needle haystack = haystack =~ regex
+                               where regex = "([^1-9])0?" ++ needle ++ "([^1-9])"
+    
+                               
